2700b7cf66d1817478d137a453b04a532279a6ab
Lakshman <lakshmanTgld>
2017-09-03 18:23:34 +0900

    removed br tag

--- app/components/ResumeComponent.js
+++ app/components/ResumeComponent.js
@@ -69,7 +69,6 @@ class ResumeComponent extends Component {
               <h1 className='robotoRegFont' style={{'lineHeight' : 'initial'}}>{resume.name}</h1>
               <h5 className='robotoLightFont' style={{'color' : '#797575','lineHeight' : '2'}}>{resume.address} &nbsp;&nbsp;|&nbsp;&nbsp;{resume.email} &nbsp;&nbsp;|&nbsp;{resume.mobile}</h5>
               <About abt={resume.about}/>
-              <br />
               <Qualifications/>
               <br />
               <Skills skillSet={resume.skills}/>
